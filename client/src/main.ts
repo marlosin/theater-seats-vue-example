8e9114db7c8a586a15927fa06cf5ece298472977
Marlos Borges <marlos.borges@gmail.com>
2018-06-19 17:08:15 -0300

    Remove vue material

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,7 @@
 import Vue from 'vue'
 import App from './app.vue'
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.css'
 
 Vue.config.productionTip = false
-
-Vue.use(VueMaterial)
 
 new Vue({
   render: h => h(App)
